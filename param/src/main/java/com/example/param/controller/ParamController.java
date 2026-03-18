0e0e320460920328d37d747579133f8e1cf56a57
Natoltora123 <nhasimbolatoavina@gmail.com>
2026-03-18 16:39:50 +0300

    feat: ajout du code td2

---
+++ param/src/main/java/com/example/param/controller/ParamController.java
@@ -0,0 +1,38 @@
+package com.example.param.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class ParamController {
+
+    private List<Students> students =  new ArrayList<>();
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam String name) {
+        return "Welcome " + name;
+    }
+
+    @PostMapping("/students")
+    public String students(@RequestBody List<Students> newStudents) {
+        students.addAll(newStudents);
+
+        return students.stream()
+                .map(s -> s.getFirstname() + " " + s.getLastname() + " " + s.getAge())
+                .collect(Collectors.joining(", "));
+    }
+
+    @GetMapping("/students")
+    public String getStudents(@RequestHeader ("Accept") String accept) {
+        if (!accept.equals("text/plain")) {
+            return "Format non supporté.";
+        }
+
+        return students.stream()
+                .map(s -> s.getFirstname() + " " + s.getLastname() + " " + s.getAge())
+                .collect(Collectors.joining(", "));
+    }
+}
---
+++ param/src/main/java/com/example/param/controller/Students.java
@@ -0,0 +1,40 @@
+package com.example.param.controller;
+
+public class Students {
+    private String reference;
+    private String Firstname;
+    private String Lastname;
+    private Integer age;
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
